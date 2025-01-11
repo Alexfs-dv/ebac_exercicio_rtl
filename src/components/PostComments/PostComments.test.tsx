b3b0a7cca49bc1d70fcf1109ff9e113d0c7d77cf
Alex <alexfaustinosouza@gmail.com>
2025-01-11 10:42:17 -0400

    atualizacao

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,20 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import { render, screen } from '@testing-library/react';
 import PostComment from '.';
+import Comment from '../../models/Comment';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+        render(<PostComment />);
+        expect(screen.getByTestId('comment-form')).toBeInTheDocument(); 
+    });
+
+    it('Deve renderizar os comentários corretamente', () => {
+        const comments = [
+            new Comment(1, "Este é o primeiro comentário."),
+            new Comment(2, "este é o segundo comentário.")
+        ];
+        render(<PostComment comments={comments} />);
+        expect(screen.getByTestId('comment-1')).toBeInTheDocument(); 
+        expect(screen.getByTestId('comment-2')).toBeInTheDocument();
     });
 });
